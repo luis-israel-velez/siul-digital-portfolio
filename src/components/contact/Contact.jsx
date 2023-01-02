892a4b76b28c23f782b502acbda0a5d433e9c041
luis.israel.velez@gmail.com <luis.israel.velez@gmail.com>
2023-01-02 16:55:56 -0500

    Removing contact sheet till I resolve serverless .env

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,6 @@ import {GrMail} from 'react-icons/gr';
 import emailjs from '@emailjs/browser';
 
 
-
 const Contact = () => {
     const form = useRef(); 
 
@@ -32,12 +31,14 @@ const Contact = () => {
                         <a href="mailto:luis.israel.velez@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
                     </article>
                 </div>
+                {/*
                 <form method="post" onSubmit={sendEmail} ref={form} id="contact-form">
                     <input type="text" name='name' id='name' placeholder='Your Full Name' required />
                     <input type="email" name='email' id='email' placeholder='Your Email' required />
                     <textarea type="message" row='7' name='message' id='message' placeholder="Your Message" required/>
                     <button type="submit" className="btn btn-primary" >Send Message</button>
                 </form>
+                /*/}
             </div>
         </section>
     )
